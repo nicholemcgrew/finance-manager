cda02152baf9b8c752c7d48af300af6dac4f757b
Nichole McGrew <nicholemcgrew@gmail.com>
2024-05-21 10:36:47 -0500

    created api route

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,40 @@
+import { z } from "zod"
+import { zValidator } from "@hono/zod-validator";
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+
+export const runtime = 'edge';
+
+const app = new Hono().basePath('/api')
+
+app
+    .get('/hello', (c) => {
+  return c.json({
+    message: 'Hello Next.js!',
+  })
+})
+.get(
+    "/hello/:test",
+     (c) => {
+    const test = c.req.param("test")
+
+    return c.json({
+        message: "hello world",
+        test: test,
+    })
+})
+.post(
+    "/",
+    zValidator("json", z.object({
+        name: z.string(),
+        userId: z.number(),
+    })),
+    (c) => {
+        const {name, userId} = c.req.valid("json")
+
+        return c.json({})
+    }
+)
+
+export const GET = handle(app)
+export const POST = handle(app)
